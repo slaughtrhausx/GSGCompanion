1d4487828711e51896118476d71a95d6926cf0de
slaughtrhaus <seanmbartley@gmail.com>
2022-12-06 14:59:24 -0800

    Initial Commit

---
+++ GSGCompanion/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GSGCompanion
+//
+//  Created by Sean Bartley on 12/6/22.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GSGCompanion/GSGCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSGCompanionApp.swift
+//  GSGCompanion
+//
+//  Created by Sean Bartley on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct GSGCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
